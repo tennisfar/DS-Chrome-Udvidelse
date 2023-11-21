2375ef53e8062f44484be8debfc620f2e6a1cf6b
Michael L Petersen <ekmlpe@danskespil.dk>
2023-11-21 15:28:16 +0100

    add toggles

---
+++ src/background.js
@@ -0,0 +1,14 @@
+const showNotification = {
+  danskeSpil: true,
+  jiraDli: true,
+  jiraDlo: true,
+  sitecoreProd: true,
+  sitecoreTown21: true,
+  sitecoreDevelop: true,
+  sitecoreTrunk: true,
+  jira: true,
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ showNotification })
+})
