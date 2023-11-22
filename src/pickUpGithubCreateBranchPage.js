feb38a8127e78a35aa204c170f1e777a977cacc1
Michael L Petersen <ekmlpe@danskespil.dk>
2023-11-22 10:18:35 +0100

    fix create branch

--- src/pickUpGithubCreateBranchPage.js
+++ src/pickUpGithubCreateBranchPage.js
@@ -18,7 +18,20 @@
       const cta = await waitForElement('[partial-name="create-branch-button"] button');
       cta?.click();
       const input = await waitForElement('[role=dialog][data-focus-trap="active"] input');
-      if (input) input.value = newBranch;
+      if (input) {
+
+        setTimeout(() => {
+          // Set the value attribute directly
+          input.setAttribute('value', newBranch);
+          
+          // Dispatch events
+          const eventsToDispatch = ['input', 'change', 'keyup'];
+          eventsToDispatch.forEach(eventType => {
+            const event = new Event(eventType, { bubbles: true, cancelable: true });
+            input.dispatchEvent(event);
+          });
+        }, 500);
+      }
     }
   };
 
