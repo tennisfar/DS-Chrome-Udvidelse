c780d866cd1def2c4ca64e271f2dee6414463a2e
Michael L Petersen <ekmlpe@danskespil.dk>
2023-11-30 12:54:50 +0100

    add settings functionality

---
+++ src/Settings.js
@@ -0,0 +1,96 @@
+(() => {
+  const handleDelete = (elName, showSettings, showButtons) => {
+    const el = document.getElementById(elName);
+
+    if (!showSettings) {
+      el.style.display = showButtons[elName] ? 'block' : 'none';
+      return;
+    }
+
+    if (showSettings) {
+      el.classList.toggle('is-danger', !showButtons[elName]);
+
+      el.querySelector('button.delete').addEventListener('click', (ev) => {
+        ev.stopPropagation();
+        showButtons[elName] = !showButtons[elName];
+        chrome.storage.sync.set({ showButtons });
+        location.reload();
+      });
+    }
+  };
+
+  const handleHideBlock = (elNames, blockEl, showSettings, showButtons) => {
+    if (showSettings) return;
+    let hideBlock = true;
+
+    elNames.forEach((elName) => {
+      if (showButtons[elName]) hideBlock = false;
+    });
+
+    if (hideBlock) {
+      document.getElementById(blockEl).style.display = 'none';
+    }
+
+    return hideBlock;
+  };
+
+  chrome.storage.sync.get('showSettings', ({ showSettings }) => {
+    const content = document.querySelector('.is-ancestor');
+    content.classList.toggle('show-settings', showSettings);
+
+    if (showSettings) {
+      const disabledButtons = document.querySelectorAll('.button[disabled]');
+      disabledButtons.forEach((button) => {
+        button.removeAttribute('disabled');
+      });
+    }
+
+    document.getElementById('settingsCta').addEventListener('click', () => {
+      showSettings = !showSettings;
+      chrome.storage.sync.set({ showSettings });
+      location.reload();
+    });
+
+    chrome.storage.sync.get('showButtons', ({ showButtons }) => {
+      const danskeSpilButtons = ['showSitecoreComponents', 'openInSitecore', 'openInExperienceEditor'];
+      danskeSpilButtons.forEach((button) => handleDelete(button, showSettings, showButtons));
+      const hideDanskeSpilButtons = handleHideBlock(danskeSpilButtons, 'danskeSpilButtons', showSettings, showButtons);
+
+      const jiraButtons = [
+        'openMyJira',
+        'openJiraBoardDli',
+        'openJiraBoardDlo',
+        'makeJiraDli',
+        'makeJiraDlo',
+        'makeJira',
+      ];
+      jiraButtons.forEach((button) => handleDelete(button, showSettings, showButtons));
+      const hideJiraButtons = handleHideBlock(jiraButtons, 'jiraButtons', showSettings, showButtons);
+
+      const sitecoreButtons = [
+        'openSitecoreProdDli',
+        'openSitecoreProdDlo',
+        'openSitecoreTown21Dli',
+        'openSitecoreTown21Dlo',
+        'openSitecoreDev',
+        'openSitecoreTrunk',
+      ];
+      sitecoreButtons.forEach((button) => handleDelete(button, showSettings, showButtons));
+      const hideSitecoreButtons = handleHideBlock(sitecoreButtons, 'sitecoreButtons', showSettings, showButtons);
+
+      const hiddenBlocks = [hideDanskeSpilButtons, hideJiraButtons, hideSitecoreButtons].filter(
+        (value) => value === true
+      ).length;
+
+      const container = document.getElementById('container');
+      if (hiddenBlocks === 0) {
+        container.style.width = '769px';
+      } else {
+        container.style.width = '300px';
+      }
+
+      // const debug = document.querySelector('#debug');
+      // debug.innerHTML = JSON.stringify(hiddenBlocks, null, 2);
+    });
+  });
+})();
--- src/background.js
+++ src/background.js
@@ -1,18 +1,28 @@
-const showNotification = {
-  danskeSpil: true,
-  jiraDli: true,
-  jiraDlo: true,
-  sitecoreProd: true,
-  sitecoreTown21: true,
-  sitecoreDevelop: true,
-  sitecoreTrunk: true,
-  jira: true,
+const showButtons = {
+  showSitecoreComponents: true,
+  openInSitecore: true,
+  openMyJira: true,
+  openInExperienceEditor: true,
+  openJiraBoardDli: true,
+  openJiraBoardDlo: true,
+  makeJiraDli: true,
+  makeJiraDlo: true,
+  makeJira: true,
+  openSitecoreProdDli: true,
+  openSitecoreProdDlo: true,
+  openSitecoreTown21Dli: true,
+  openSitecoreTown21Dlo: true,
+  openSitecoreDev: true,
+  openSitecoreTrunk: true,
 };
+
+let showSettings = false;
 
 let favorites = [];
 
 chrome.runtime.onInstalled.addListener((reason) => {
-  chrome.storage.sync.set({ showNotification });
+  chrome.storage.sync.set({ showButtons });
+  chrome.storage.sync.set({ showSettings });
 
   if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.storage.sync.set({ favorites });
