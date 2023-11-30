2073c14d74f8adf7979a5206168cd24d86c528df
Michael L Petersen <ekmlpe@danskespil.dk>
2023-11-30 12:54:11 +0100

    fix version check so it works for semantic versioning

--- src/CurrentVersion.js
+++ src/CurrentVersion.js
@@ -3,19 +3,31 @@
   const localDataUrl = '../manifest.json';
   const gitHubApiUrl = 'https://api.github.com/repos/tennisfar/ds-chrome-extension/releases';
 
+  function isVersionLower(userVersion, newVersion) {
+    const userParts = userVersion.split('.').map(Number);
+    const newParts = newVersion.split('.').map(Number);
+
+    for (let i = 0; i < Math.max(userParts.length, newParts.length); i++) {
+      const userPart = userParts[i] || 0;
+      const newPart = newParts[i] || 0;
+
+      if (userPart < newPart) return true;
+      if (userPart > newPart) return false;
+    }
+    return false;
+  }
+
   try {
     const [localDataResponse, gitHubResponse] = await Promise.all([fetch(localDataUrl), fetch(gitHubApiUrl)]);
-
     const localData = await localDataResponse.json();
     const releases = await gitHubResponse.json();
     const latestRelease = releases[0]; // The first item in the array is the latest release
     const latestVersion = latestRelease?.tag_name?.replace('v', '');
-    const localVersion = `v${localData.version}`;
-
     let url, message;
-    if (latestRelease?.html_url && Number(latestVersion) > Number(localData.version)) {
+
+    if (latestRelease?.html_url && isVersionLower(localData.version, latestVersion)) {
       url = latestRelease.html_url;
-      message = `Du kan opdatere fra version ${localData.version} til ${latestVersion}`;
+      message = `Opdater til v. ${latestVersion} (du har ${localData.version})`;
     } else {
       url = 'https://github.com/tennisfar/ds-chrome-extension/releases';
       message = `Version ${localData.version}`;
