af184ad709c88e2325ddfb9a924b8906f186c09e
Michael L Petersen <ekmlpe@danskespil.dk>
2023-11-30 18:05:20 +0100

    fix finding text in dictionaries

--- src/FindDictionary.js
+++ src/FindDictionary.js
@@ -1,4 +1,172 @@
 (() => {
+  const findTextInDictionaries = () => {
+    const txt = window.getSelection().toString();
+
+    if (txt?.length > 4) {
+      let results = {};
+
+      const output = ({ key, key2, key3, key4, key5, key6, key7, key8, res, dictionaryKey }) => {
+        key = key ? key + '/' : '';
+        key2 = key2 ? key2 + '/' : '';
+        key3 = key3 ? key3 + '/' : '';
+        key4 = key4 ? key4 + '/' : '';
+        key5 = key5 ? key5 + '/' : '';
+        key6 = key6 ? key6 + '/' : '';
+        key7 = key7 ? key7 + '/' : '';
+        key8 = key8 ? key8 + '/' : '';
+
+        const isDlo = dictionaryKey.includes('-DLO-');
+
+        const region = dictionaryKey.split('path=/')[1].split('&')[0] + '/';
+
+        results[region + key2 + key3 + key4 + key5 + key6 + key7 + key8] = {
+          isDlo,
+          res,
+        };
+      };
+
+      let indexKey = 0;
+      const dictionaryKeys = [];
+
+      while (localStorage.key(indexKey)) {
+        const storageKey = localStorage.key(indexKey);
+
+        if (storageKey.includes('/dlo/scapi/common/dictionary/dictionary')) {
+          dictionaryKeys.push(storageKey);
+        }
+
+        indexKey++;
+      }
+
+      dictionaryKeys.forEach((dictionaryKey) => {
+        const s = JSON.parse(localStorage.getItem(dictionaryKey));
+
+        if (typeof s !== 'object') return;
+
+        Object.keys(s).forEach((key) => {
+          if (!s[key]) return;
+
+          if (typeof s[key] === 'string')
+            if (s[key].includes(txt))
+              output({
+                key,
+                res: s[key],
+                dictionaryKey,
+              });
+
+          Object.keys(s[key]).forEach((key2) => {
+            if (typeof s[key][key2] === 'string')
+              if (s[key][key2].includes(txt))
+                output({
+                  key,
+                  key2,
+                  res: s[key][key2],
+                  dictionaryKey,
+                });
+
+            Object.keys(s[key][key2]).forEach((key3) => {
+              if (typeof s[key][key2][key3] === 'string')
+                if (s[key][key2][key3].includes(txt))
+                  output({
+                    key,
+                    key2,
+                    key3,
+                    res: s[key][key2][key3],
+                    dictionaryKey,
+                  });
+
+              Object.keys(s[key][key2][key3]).forEach((key4) => {
+                if (typeof s[key][key2][key3][key4] === 'string')
+                  if (s[key][key2][key3][key4].includes(txt))
+                    output({
+                      key,
+                      key2,
+                      key3,
+                      key4,
+                      res: s[key][key2][key3][key4],
+                      dictionaryKey,
+                    });
+
+                Object.keys(s[key][key2][key3][key4]).forEach((key5) => {
+                  if (typeof s[key][key2][key3][key4][key5] === 'string')
+                    if (s[key][key2][key3][key4][key5].includes(txt))
+                      output({
+                        key,
+                        key2,
+                        key3,
+                        key4,
+                        key5,
+                        res: s[key][key2][key3][key4][key5],
+                        dictionaryKey,
+                      });
+
+                  Object.keys(s[key][key2][key3][key4][key5]).forEach((key6) => {
+                    if (typeof s[key][key2][key3][key4][key5][key6] === 'string')
+                      if (s[key][key2][key3][key4][key5][key6].includes(txt))
+                        output({
+                          key,
+                          key2,
+                          key3,
+                          key4,
+                          key5,
+                          key6,
+                          res: s[key][key2][key3][key4][key5][key6],
+                          dictionaryKey,
+                        });
+
+                    Object.keys(s[key][key2][key3][key4][key5][key6]).forEach((key7) => {
+                      if (typeof s[key][key2][key3][key4][key5][key6][key7] === 'string')
+                        if (s[key][key2][key3][key4][key5][key6][key7].includes(txt))
+                          output({
+                            key,
+                            key2,
+                            key3,
+                            key4,
+                            key5,
+                            key6,
+                            key7,
+                            res: s[key][key2][key3][key4][key5][key6][key7],
+                            dictionaryKey,
+                          });
+
+                      Object.keys(s[key][key2][key3][key4][key5][key6][key7]).forEach((key8) => {
+                        if (typeof s[key][key2][key3][key4][key5][key6][key7][key8] === 'string')
+                          if (s[key][key2][key3][key4][key5][key6][key7][key8].includes(txt))
+                            output({
+                              key,
+                              key2,
+                              key3,
+                              key4,
+                              key5,
+                              key6,
+                              key7,
+                              key8,
+                              res: s[key][key2][key3][key4][key5][key6][key7][key8],
+                              dictionaryKey,
+                            });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+
+      const content = [];
+      if (Object.keys(results).length > 150) return;
+      Object.keys(results).forEach((key) => {
+        const url = getDictionaryUrl(results[key].isDlo) + key;
+        content.push({ txt: results[key].res, url, key });
+      });
+
+      fillDictionary(content);
+    } else {
+      clearFillDictionary();
+    }
+  };
+
   const getDictionaryUrl = (isDlo) => {
     const hostParts = location.host.split('.');
     const protocol = location.protocol + '//';
@@ -8,57 +176,7 @@
 
     return `${protocol}${envPart}.danskespil.dk/sitecore/shell/Applications/Content%20Editor.aspx?sc_bw=1&fo=/sitecore/content/DanskeSpil/Site%20settings/Dictionary/`;
   };
-
-  const findDictionary = () => {
-    const txt = window.getSelection().toString().toLowerCase();
-
-    if (!txt || txt.length <= 4) {
-      clearFillDictionary();
-      return;
-    }
-
-    let results = {};
-
-    const output = (keys, res, dictionaryKey) => {
-      const path = keys.map((k) => (k ? k + '/' : '')).join('');
-      const isDlo = dictionaryKey.includes('-DLO-');
-      const region = dictionaryKey.split('path=/')[1].split('&')[0] + '/';
-
-      results[region + path] = { isDlo, res };
-    };
-
-    const searchKeys = (obj, currentKeys = [], dictionaryKey) => {
-      if (typeof obj !== 'object' || obj === null) return;
-
-      Object.entries(obj).forEach(([key, value]) => {
-        const newKeys = [...currentKeys, key];
-        if (typeof value === 'string' && value.toLowerCase().includes(txt)) {
-          output(newKeys, value, dictionaryKey);
-        } else {
-          searchKeys(value, newKeys, dictionaryKey);
-        }
-      });
-    };
-
-    const dictionaryKeys = Array.from({ length: localStorage.length })
-      .map((_, i) => localStorage.key(i))
-      .filter((key) => key.includes('/dlo/scapi/common/dictionary/dictionary'));
-
-    dictionaryKeys.forEach((dictionaryKey) => {
-      const s = JSON.parse(localStorage.getItem(dictionaryKey));
-      searchKeys(s, [], dictionaryKey);
-    });
-
-    if (Object.keys(results).length > 150) return;
-
-    const content = Object.entries(results).map(([key, { res, isDlo }]) => {
-      const url = getDictionaryUrl(isDlo) + key;
-      return { txt: res, url, key };
-    });
-
-    fillDictionary(content);
-  };
-
+  
   const clearFillDictionary = () => {
     const removeElementById = (id) => {
       const el = document.getElementById(id);
@@ -171,6 +289,6 @@
   };
 
   if (location.host.endsWith('danskespil.dk') && !location.pathname.startsWith('/sitecore/')) {
-    document.addEventListener('selectionchange', findDictionary);
+    document.addEventListener('selectionchange', findTextInDictionaries);
   }
 })();
